30eeb70a8d5c841f8d562edc432f95d1184a386c
PureOne1111 <pauln9990@gmail.com>
2025-12-29 15:24:12 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hyper-lattice-protocol",
+    version="9.0.1",
+    packages=find_packages(),
+    author="Omni-Manifold Committee",
+    description="AGI Alignment Kernels",
+)
